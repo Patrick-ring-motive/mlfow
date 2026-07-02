f9c9b72c9680019fffb14a0c11fdf3549237ebf0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-02 17:49:21 -0500

    style: js-beautify formatting (script.js)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-(()=>{
+(() => {
   const style = document.createElement('style');
   style.innerHTML = `
   #runllm-widget{
     display:none !important;
   }`;
-  (document.body||document.firstElementChild).appendChild(style);
+  (document.body || document.firstElementChild).appendChild(style);
 })();
